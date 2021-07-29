f4c5ddf70b191158493fad999d218dbddd466fe3
PeterPetrik <zilolv@gmail.com>
2021-07-29 16:32:10 +0200

    10.0.2 theme nuxtjs content theme is broken

--- www/nuxt.config.js
+++ www/nuxt.config.js
@@ -2,6 +2,21 @@ import theme from '@nuxt/content-theme-docs'
 
 export default theme({
   docs: {
-    primaryColor: '#246c55'
+    // primaryColor: '#E24F55'
+  },
+  loading: { color: '#00CD81' },
+  i18n: {
+    locales: () => [{
+      code: 'fr',
+      iso: 'fr-FR',
+      file: 'fr-FR.js',
+      name: 'Français'
+    }, {
+      code: 'en',
+      iso: 'en-US',
+      file: 'en-US.js',
+      name: 'English'
+    }],
+    defaultLocale: 'en'
   }
 })
