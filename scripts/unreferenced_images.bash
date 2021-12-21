75260d377edc0758c6e8c73af726b7f0e1be3f10
PeterPetrik <zilolv@gmail.com>
2021-12-21 12:10:57 +0100

    check for unused XCF files too

--- scripts/unreferenced_images.bash
+++ scripts/unreferenced_images.bash
@@ -3,9 +3,11 @@ IFS=$'\n'
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SRC_DIR=$SCRIPT_DIR/../src
 USED_PICS=0
+USED_XCF=0
+NOT_USED_XCF=0
 NOT_USED_PICS=0
 
-PICS=`find src/ | grep -e png -e jpg -e svg`
+PICS=`find src/ | grep -e png -e jpg -e svg -e PNG -e JPG -e SVG -e jpeg -e JPEG`
 stringarray=($PICS)
 
 for i in "${stringarray[@]}"
@@ -21,5 +23,30 @@ do
   fi
 done
 
-echo "number or errors: $NOT_USED_PICS"
-exit $NOT_USED_PICS
+XCF=`find src/ | grep -e xcf`
+stringarray=($XCF)
+
+for i in "${stringarray[@]}"
+do 
+  bi="$(basename -- $i)"
+  biwe=${bi%xcf} # Remove file extension
+  
+  A2=`grep -rn "$biwe" $SRC_DIR --include \*.md --include \*.vue`
+  if [ ! -z "$A2" ]
+  then
+   USED_XCF=$((USED_XCF + 1))
+  else
+   echo "$i is not needed anymore"
+   NOT_USED_XCF=$((NOT_USED_XCF + 1))
+  fi
+done
+
+ERRORS=$((NOT_USED_PICS + NOT_USED_XCF))
+
+echo "number of used pictures: $USED_PICS"
+echo "number of not used XCF files: $USED_XCF"
+echo "number of not used pictures: $NOT_USED_PICS"
+echo "number of not used XCF: $NOT_USED_XCF"
+
+echo "number of errors: $ERRORS"
+exit $ERRORS
