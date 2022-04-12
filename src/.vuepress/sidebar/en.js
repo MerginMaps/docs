71d98478f4086f6cbdfa121ebe328d0414c59f48
PeterPetrik <zilolv@gmail.com>
2022-04-12 08:48:05 +0200

    add link na rebrand blog

--- src/.vuepress/sidebar/en.js
+++ src/.vuepress/sidebar/en.js
@@ -1,5 +1,6 @@
 module.exports = {
       '/': [
+        ['https://www.lutraconsulting.co.uk/blog/2022/04/18/merginmaps/', 'We are rebranding!'],
         {
             title: 'Get started',   // required
             collapsable: true, // optional, defaults to true
