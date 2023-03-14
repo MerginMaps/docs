496af535853de928ac55df2f19414c4150740a5a
Peter Petrik <zilolv@gmail.com>
2023-03-14 11:29:59 +0100

    fix rendering on safari

--- src/.vuepress/components/SinceBadge.vue
+++ src/.vuepress/components/SinceBadge.vue
@@ -1,7 +1,9 @@
 <template>
+  <span>
   <span class="sincebadge" v-if="type === 'App'" >since Input App {{ version }}</span>
   <span class="sincebadge" v-else-if="type === 'Plugin'">since Plugin {{ version }}</span>
   <span class="sincebadge" v-else-if="type === 'Server'">since Server {{ version }}</span>
+  </span>
 </template>
 
 
@@ -17,17 +19,13 @@ export default {
 
 <style lang="stylus" scoped>
 .sincebadge
-  vertical-align top
   display inline-block
-  font-size 14px
   height 18px
   line-height 18px
   border-radius 3px
   padding 0 6px
   color white
-  background-color #42b983
   background-color $badgeTipColor
   margin-left 5px
 </style>
-
 
