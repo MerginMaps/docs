b43645973c1bb954b6c11c5d96fe59223920c28c
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2023-06-23 14:36:48 +0200

    reordering columns

--- src/.vuepress/theme/components/PageFooter.vue
+++ src/.vuepress/theme/components/PageFooter.vue
@@ -2,16 +2,16 @@
   <footer class="page-footer">
     <div class="page-footer__container">
       <div class="page-footer__row">
+				<div class="page-footer__col">
+					<img src="/docs/mm_logo_positive_half_padding.png" />
+					<p>Copyright &copy; 2021 - {{ year }}</p>
+					<p>Lutra Consulting Limited</p>
+					<p>All rights reserved.</p>
+				</div>
         <div class="page-footer__col">
           <h4>Company</h4>
           <a href="https://merginmaps.com" target="__blank">Homepage</a>
           <a href="https://merginmaps.com/start-for-free" target="__blank">Start for free</a>
-        </div>
-        <div class="page-footer__col">
-          <img src="/docs/mm_logo_positive_half_padding.png" />
-          <p>Copyright &copy; 2021 - {{ year }}</p>
-					<p>Lutra Consulting Limited</p>
-          <p>All rights reserved.</p>
         </div>
       </div>
       <div class="page-footer__center">
@@ -21,10 +21,10 @@
           <SocialIconsLink icon="github" class="page-footer__icon" />
         </div>
         <div>
-            <a href="https://merginmaps.com/cookies" target="__blank">Cookie policy</a> &#x2022
-            <a href="https://merginmaps.com/privacy-policy" target="__blank">Privacy policy</a> &#x2022
-            <a href="https://merginmaps.com/licenses/terms-of-service" target="__blank">Terms of service</a>
-          </div>
+					<a href="https://merginmaps.com/cookies" target="__blank">Cookie policy</a> &#x2022
+					<a href="https://merginmaps.com/privacy-policy" target="__blank">Privacy policy</a> &#x2022
+					<a href="https://merginmaps.com/licenses/terms-of-service" target="__blank">Terms of service</a>
+        </div>
       </div>
     </div>
   </footer>
@@ -74,7 +74,6 @@ export default Vue.extend({
     display: flex;
     flex-direction: column;
     margin-bottom: 1rem;
-    width: 33%;
 
     img {
       height: 2.2rem;
