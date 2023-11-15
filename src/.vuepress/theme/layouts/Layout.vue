51ef5c4acf8d876992bcf41b7acfe055eb03525d
PeterPetrik <zilolv@gmail.com>
2023-11-15 07:43:47 +0100

    translate

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <ParentLayout
-      ><template #page-bottom><PageFooter /></template></ParentLayout
-    >
+    <ParentLayout>
+       <template #sidebar-top><PageHeader /></template> 
+       <template #page-bottom><PageFooter /></template>
+    </ParentLayout>
   </div>
 </template>
 
 <script>
 import PageFooter from "@theme/components/PageFooter.vue";
+import PageHeader from "@theme/components/PageHeader.vue";
 import ParentLayout from "@parent-theme/layouts/Layout.vue";
 
 export default {
   components: {
     ParentLayout,
-    PageFooter
+    PageFooter,
+    PageHeader
   },
 };
 </script>
