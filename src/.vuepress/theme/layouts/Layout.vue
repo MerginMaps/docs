6f8788f39f6741907c7dc64f58f46a7a462c1d7d
Peter Petrik <zilolv@gmail.com>
2023-11-15 07:13:52 +0100

    Revert "add banner for non-english versions"

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
-    <ParentLayout>
-       <template #sidebar-top><PageHeader /></template> 
-       <template #page-bottom><PageFooter /></template>
-    </ParentLayout>
+    <ParentLayout
+      ><template #page-bottom><PageFooter /></template></ParentLayout
+    >
   </div>
 </template>
 
 <script>
 import PageFooter from "@theme/components/PageFooter.vue";
-import PageHeader from "@theme/components/PageHeader.vue";
 import ParentLayout from "@parent-theme/layouts/Layout.vue";
 
 export default {
   components: {
     ParentLayout,
-    PageFooter,
-    PageHeader
+    PageFooter
   },
 };
 </script>
