e1dbb0863967aa284f99035022804e92f3e3590d
Alexandra <alexandra.rasova@lutraconsulting.co.uk>
2025-03-28 19:01:18 +0100

    rename pages in sidebar

--- src/.vitepress/sidebar/en.js
+++ src/.vitepress/sidebar/en.js
@@ -2,8 +2,9 @@ export default {
   '/': [
     {
       text: 'Get Started',   // required
+      collapsed: false,
       items: [
-        { text: 'Capturing First Data', link: '/tutorials/capturing-first-data/' },
+        { text: 'Capturing Your First Field Data', link: '/tutorials/capturing-first-data/' },
         { text: 'Opening Surveyed Data on Your Computer', link: '/tutorials/opening-surveyed-data-on-your-computer/' },
         { text: 'Creating a Project in QGIS', link: '/tutorials/creating-a-project-in-qgis/' },
         { text: 'Using Mergin Maps Mobile App', link: '/tutorials/mobile/' },
@@ -13,77 +14,82 @@ export default {
     },
     {
       text: 'Install & Sign Up',   // required
+      collapsed: false,
       items: [
-        { text: 'Install Mobile App', link: '/setup/install-mobile-app/' },
-        { text: 'Sign Up to Mergin Maps', link: '/setup/sign-up-to-mergin-maps/' },
-        { text: 'Install QGIS', link: '/setup/install-qgis/' },
-        { text: 'Install Mergin Maps Plugin for QGIS', link: '/setup/install-mergin-maps-plugin-for-qgis/' }
+        { text: 'How to Install Mergin Maps Mobile App', link: '/setup/install-mobile-app/' },
+        { text: 'How to Sign Up to Mergin Maps', link: '/setup/sign-up-to-mergin-maps/' },
+        { text: 'How to Install QGIS', link: '/setup/install-qgis/' },
+        { text: 'How to Install Mergin Maps Plugin for QGIS', link: '/setup/install-mergin-maps-plugin-for-qgis/' }
       ]
     },
     {
       text: 'Manage Account & Project',   // required
+      collapsed: false,
       items: [
-        { text: 'Account', link: '/manage/account/' },
+        { text: 'User Account', link: '/manage/account/' },
         { text: 'Workspaces', link: '/manage/workspaces/' },
-        { text: 'Subscriptions', link: '/manage/subscriptions/' },
-        { text: 'Permissions', link: '/manage/permissions/' },
+        { text: 'Subscriptions and Invoicing', link: '/manage/subscriptions/' },
+        { text: 'Member Roles and Permissions', link: '/manage/permissions/' },
         { text: 'Synchronisation', link: '/manage/synchronisation/' },
-        { text: 'Project', link: '/manage/project/' },
-        { text: 'Create Project', link: '/manage/create-project/' },
-        { text: 'Project Advanced', link: '/manage/project-advanced/' },
-        { text: 'Delete Files', link: '/manage/delete-files/' },
-        { text: 'Deploy New Project', link: '/manage/deploy-new-project/' },
-        { text: 'Missing Data', link: '/manage/missing-data/' },
-        { text: 'Plugin', link: '/manage/plugin/' },
-        { text: 'Dashboard', link: '/manage/dashboard/' },
-        { text: 'Dashboard Maps', link: '/manage/dashboard-maps/' },
-        { text: 'Project History', link: '/manage/project-history/' },
-        { text: 'Selective Sync', link: '/manage/selective_sync/' }
+        { text: 'Mergin Maps Project', link: '/manage/project/' },
+        { text: 'How to Create a New Project', link: '/manage/create-project/' },
+        { text: 'How to Share, Transfer or Delete Projects', link: '/manage/project-advanced/' },
+        { text: 'How to Delete Files', link: '/manage/delete-files/' },
+        { text: 'How to Deploy Revised Projects', link: '/manage/deploy-new-project/' },
+        { text: 'How to Recover Missing Data', link: '/manage/missing-data/' },
+        { text: 'Mergin Maps QGIS Plugin Overview', link: '/manage/plugin/' },
+        { text: 'Mergin Maps Dashboard', link: '/manage/dashboard/' },
+        { text: 'Webmaps', link: '/manage/dashboard-maps/' },
+        { text: 'Project History and Versions', link: '/manage/project-history/' },
+        { text: 'Selective Synchronisation', link: '/manage/selective_sync/' }
       ]
     },
     {
       text: 'Setup GIS Project',   // required
+      collapsed: false,
       items: [
-        { text: 'Features', link: '/gis/features/' },
-        { text: 'Search Data', link: '/gis/search_data/' },
-        { text: 'Setting Up Background Map', link: '/gis/settingup_background_map/' },
-        { text: 'Setup Themes', link: '/gis/setup_themes/' },
-        { text: 'Photo Names', link: '/gis/photo-names/' },
-        { text: 'Enable Digitising', link: '/gis/enable_digitising/' },
-        { text: 'Snapping', link: '/gis/snapping/' },
-        { text: 'Proj', link: '/gis/proj/' },
+        { text: 'QGIS Project Preparation', link: '/gis/features/' },
+        { text: 'Sorting and Search Setup', link: '/gis/search_data/' },
+        { text: 'Background Maps', link: '/gis/settingup_background_map/' },
+        { text: 'Map Themes', link: '/gis/setup_themes/' },
+        { text: 'How to Set Photo Names Format', link: '/gis/photo-names/' },
+        { text: 'How to Enable Digitising', link: '/gis/enable_digitising/' },
+        { text: 'How to Set Up Snapping for Mergin Maps Mobile App', link: '/gis/snapping/' },
+        { text: 'Custom Projections', link: '/gis/proj/' },
         { text: 'Supported Formats', link: '/gis/supported_formats/' }
       ]
     },
     {
       text: 'Configure Survey Layer',   // required
+      collapsed: false,
       items: [
-        { text: 'Best Practice', link: '/layer/best-practice/' },
-        { text: 'Form Widgets', link: '/layer/form-widgets/' },
-        { text: 'Form Configuration', link: '/layer/form-configuration/' },
-        { text: 'Form Layout', link: '/layer/form-layout/' },
-        { text: 'Photos', link: '/layer/photos/' },
-        { text: 'Attach Multiple Photos to Features', link: '/layer/attach-multiple-photos-to-features/' },
-        { text: 'One-to-N Relations', link: '/layer/one-to-n-relations/' },
-        { text: 'EXIF', link: '/layer/exif/' },
-        { text: 'External Link', link: '/layer/external-link/' },
-        { text: 'Non-Spatial Data', link: '/layer/non-spatial-data/' },
-        { text: 'Position Variables', link: '/layer/position_variables/' },
-        { text: 'Plugin Variables', link: '/layer/plugin-variables/' }
+        { text: 'Best Practice Tips for Layers and Forms', link: '/layer/best-practice/' },
+        { text: 'Setting Up Widgets in Attributes Form', link: '/layer/form-widgets/' },
+        { text: 'Attributes Form Configuration', link: '/layer/form-configuration/' },
+        { text: 'Attributes Form Layout', link: '/layer/form-layout/' },
+        { text: 'Capturing Photos', link: '/layer/photos/' },
+        { text: 'How to Attach Multiple Photos to Features', link: '/layer/attach-multiple-photos-to-features/' },
+        { text: 'How to Link Multiple Records to One Feature (1-N Relations)', link: '/layer/one-to-n-relations/' },
+        { text: 'Exif Metadata', link: '/layer/exif/' },
+        { text: 'How to Use Hyperlinks', link: '/layer/external-link/' },
+        { text: 'Working with Non-Spatial Tables', link: '/layer/non-spatial-data/' },
+        { text: 'Extra Position Variables', link: '/layer/position_variables/' },
+        { text: 'Extra QGIS Variables', link: '/layer/plugin-variables/' }
       ]
     },
     {
       text: 'Fieldwork Tips',   // required
+      collapsed: false,
       items: [
         { text: 'Mergin Maps Mobile App Interface', link: '/field/mobile-app-ui/' },
-        { text: 'Offline Use', link: '/field/offline-use/' },
+        { text: 'Offline Use of Mergin Maps Mobile App', link: '/field/offline-use/' },
         { text: 'External GPS', link: '/field/external_gps/' },
         { text: 'GPS Accuracy', link: '/field/gps_accuracy/' },
         { text: 'Position Tracking', link: '/field/tracking/' },
         { text: 'Synchronisation in Mergin Maps Mobile App', link: '/field/autosync/' },
-        { text: 'Measure', link: '/field/measure/' },
-        { text: 'Layers', link: '/field/layers/' },
-        { text: 'Mobile Features', link: '/field/mobile-features/' },
+        { text: 'Measurement Tools', link: '/field/measure/' },
+        { text: 'Layers in Mergin Maps Mobile App', link: '/field/layers/' },
+        { text: 'How to Add, Edit, Delete Featuress', link: '/field/mobile-features/' },
         { text: 'How to Reuse Last Entered Values', link: '/field/reuse-last-values/' },
         { text: 'How to Stake Out Points', link: '/field/stake-out/' },
         { text: 'How to Fix a Broken Project', link: '/field/broken-project/' }
@@ -91,10 +97,11 @@ export default {
     },
     {
       text: 'For Developers',   // required
+      collapsed: false,
       items: [
-        { text: 'Custom App', link: '/dev/customapp/' },
-        { text: 'Integration', link: '/dev/integration/' },
-        { text: 'DB Sync', link: '/dev/dbsync/' },
+        { text: 'Custom Mobile App', link: '/dev/customapp/' },
+        { text: 'Integrate Mergin Maps', link: '/dev/integration/' },
+        { text: 'PostgreSQL DB Sync', link: '/dev/dbsync/' },
         { text: 'Media Sync', link: '/dev/media-sync/' },
         { text: 'Work Packages', link: '/dev/work-packages/' },
         { text: 'Geodiff Library', link: '/dev/geodiff/' }
@@ -102,19 +109,21 @@ export default {
     },
     {
       text: 'Custom Server',   // required
+      collapsed: false,
       items: [
         { text: 'Overview', link: '/server/' },
         { text: 'Install', link: '/server/install/' },
-        { text: 'Secure Mergin Maps installation', link: '/server/security/' },
+        { text: 'Secure Mergin Maps Installation', link: '/server/security/' },
         { text: 'Upgrade', link: '/server/upgrade/' },
         { text: 'Administer', link: '/server/administer/' },
         { text: 'Administration Panel', link: '/server/dashboard/' },
-        { text: 'Troubleshoot', link: '/server/troubleshoot/' },
-        { text: 'Plugin Mobile App', link: '/server/plugin-mobile-app/' }
+        { text: 'Troubleshoot Custom Servers', link: '/server/troubleshoot/' },
+        { text: 'Using Mergin Maps Mobile App and QGIS Plugin with a Custom Server', link: '/server/plugin-mobile-app/' }
       ]
     },
     {
       text: 'Migrate',   // required
+      collapsed: false,
       items: [
         { text: 'Migrate from QField', link: '/migrate/qfield/' },
         { text: 'Migrate from ArcGIS', link: '/migrate/arcgis/' }
@@ -122,11 +131,12 @@ export default {
     },
     {
       text: 'Support & Legal',   // required
+      collapsed: false,
       items: [
         { text: 'Licensing', link: '/misc/licensing/' },
         { text: 'Troubleshoot', link: '/misc/troubleshoot/' },
         { text: 'Contribute', link: '/misc/contribute/' },
-        { text: 'Write Docs', link: '/misc/write-docs/' }
+        { text: 'Write Documentation', link: '/misc/write-docs/' }
       ]
     }
   ]
