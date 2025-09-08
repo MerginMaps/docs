a8be6ef66b7576abc918da9f7ccc6f62017c0cb5
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2025-09-08 15:12:30 +0200

    Added og:title and og:description.. Lazy load of youtube

--- src/.vitepress/components/YouTube.vue
+++ src/.vitepress/components/YouTube.vue
@@ -1,20 +1,10 @@
 <template>
-  <div>
-    <iframe
-      type="text/html"
-      width="100%"
-      style="height: 100%"
-      src=""
-      frameborder="0"
-      allowfullscreen
-      ref="iframe"
-      :title="title"
-    ></iframe>
-  </div>
+    <lite-youtube :videoid="id" :videotitle="title" posterquality="maxresdefault"></lite-youtube>
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
+import '@justinribeiro/lite-youtube'
 
 const props = defineProps({
   id: {
@@ -26,29 +16,7 @@ const props = defineProps({
     default: "YouTube video",
   },
 });
-const iframe = ref(null);
-
-onMounted(() => {
-  if (iframe.value) {
-    iframe.value.src = `https://www.youtube.com/embed/${props.id}`;
-  }
-});
 </script>
 
 <style scoped>
-div {
-  background-color: #2c3e50;
-  width: 100%;
-  padding-top: min(500px, 56%);
-  position: relative;
-  border: 3px solid #eaecef;
-}
-
-iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-}
 </style>
